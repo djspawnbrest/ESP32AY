c1a2d980f93f8efa0b2443849e4e46869b8de9c7
djspawnbrest <djspawnbrest@gmail.com.com>
2024-10-06 21:29:36 +0300

    udate encButton library

--- lib/EncButton-3.5.4/src/EncButton.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-#include "core/VirtButton.h"
-#include "core/VirtEncoder.h"
-#include "core/VirtEncButton.h"
-#include "core/Button.h"
-#include "core/Encoder.h"
-#include "core/EncButton.h"
