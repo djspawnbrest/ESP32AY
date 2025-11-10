caf23a020820dacfe54ad7b3b3a6d38ba84de850
djspawnbrest <djspawnbrest@gmail.com>
2025-11-11 00:11:31 +0300

    update postScripts and fw bin files.

--- scripts/postScript.py
+++ scripts/postScript.py
@@ -5,17 +5,17 @@ Import("env")
 firmware_dir = os.path.join(env["PROJECT_DIR"], "firmware")
 os.makedirs(firmware_dir, exist_ok=True)  # Создаём папку, если её нет
 
-# Оригинальные пути файлов прошивки
-flash_images_paths = {
-    "0x1000": os.path.join(env["BUILD_DIR"], "bootloader.bin"),
-    "0x8000": os.path.join(env["BUILD_DIR"], "partitions.bin"),
-    "0xe000": os.path.join(env["PROJECT_CORE_DIR"], "packages/framework-arduinoespressif32/tools/partitions", "boot_app0.bin"),
-    "0x10000": os.path.join(env["BUILD_DIR"], "firmware.bin"),
-}
-
 # Функция для объединения прошивок
 def merge_bin(source, target, env):
     merged_bin_path = os.path.join(firmware_dir, f"{env['PIOENV']}.bin")
+    
+    # Пути файлов прошивки (ordered list for ESP32)
+    flash_images = [
+        ("0x1000", os.path.join(env["BUILD_DIR"], "bootloader.bin")),
+        ("0x8000", os.path.join(env["BUILD_DIR"], "partitions.bin")),
+        ("0xe000", os.path.join(env["PROJECT_CORE_DIR"], "packages/framework-arduinoespressif32/tools/partitions", "boot_app0.bin")),
+        ("0x10000", os.path.join(env["BUILD_DIR"], "firmware.bin")),
+    ]
 
     # Формируем команду для объединения прошивки
     command = " ".join([
@@ -25,9 +25,9 @@ def merge_bin(source, target, env):
         "merge_bin",
         "-o", merged_bin_path,
         "--flash_mode keep",
-        "--flash_freq 80m",
+        "--flash_freq keep",
         "--flash_size keep"
-    ] + [f"{addr} {path}" for addr, path in flash_images_paths.items()])
+    ] + [f"{addr} {path}" for addr, path in flash_images])
 
     # Выполнение команды
     env.Execute(command)
